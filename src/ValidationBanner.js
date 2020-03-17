b26c985de6228ff121e02d58b50b49c158dbcf77
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-17 13:16:29 -0500

    Set text color depending on textLength in ValidationBanner.

--- src/ValidationBanner.js
+++ src/ValidationBanner.js
@@ -5,11 +5,19 @@ import './ValidationBanner.css';
 const MIN_TEXT_LENGTH = 5;
 
 function ValidationBanner(props) {
+
+  function setTextStyle() {
+    return {
+      color: props.textLength >= MIN_TEXT_LENGTH ? 'green' : 'red',
+    };
+  }
+
   return (
-    <div className="validation-banner">
+    <div className="validation-banner" style={setTextStyle()}>
       Text {props.textLength >= MIN_TEXT_LENGTH ? 'long enough' : 'too short'}
     </div>
   );
+  
 }
 
 ValidationBanner.propTypes = {
