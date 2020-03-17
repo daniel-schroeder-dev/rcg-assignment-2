5680ae86669f85aed4662ca24cbc10336cd3aa8f
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-17 13:01:43 -0500

    Setup the text input in App component to be a controlled component.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import './App.css';
 
 class App extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputText: '',
+    };
+  }
+
+  handleInputTextChange = e => {
+    this.setState({ inputText: e.target.value });
+  };
+
   render() {
     return (
       <div className="app">
         <div className="app__assignment-container">
-          <input type="text" />
+          <input type="text" value={this.state.inputText} onChange={this.handleInputTextChange} />
         </div>
         <Instructions />
       </div>
