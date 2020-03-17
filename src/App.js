f97d2833a0e752f46eb4cc5f78f30db0c7fe11b0
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2020-03-17 12:58:00 -0500

    Refactor 'App' into a class component.

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import Instructions from './Instructions';
 import './App.css';
 
-function App() {
-  return (
-    <div className="app">
-      <div className="app__assignment-container">
-        <input type="text" />
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="app">
+        <div className="app__assignment-container">
+          <input type="text" />
+        </div>
+        <Instructions />
       </div>
-      <Instructions />
-    </div>
-  );
+    );  
+  }
+  
 }
 
 export default App;
